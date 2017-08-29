8915658a192d5c6a44c9354f63cdef3c50982685
Maxime Boisvert <maxime.boisvert@shopify.com>
2017-08-29 19:02:51 -0400

    Simpler

--- js/app.js
+++ js/app.js
@@ -2,15 +2,11 @@
 main();
 
 function main() {
-  addScript('/node_modules/jquery/dist/jquery.js', function() {
-    $(document).ready(function() {
-      addAuthorMetas();
-      addBackButtons();
-      lazyLoad();
-    });
+  $(document).ready(function() {
+    addAuthorMetas();
+    addBackButtons();
+    lazyLoad();
   });
-
-  addScript('/js/analytics.js')
 }
 
 function addAuthorMetas() {
@@ -28,39 +24,8 @@ function lazyLoad() {
 }
 
 function addBackButtons() {
-  var buttons = $('.back');
-
-  buttons.append($('<a>', {
+  $('.back').append($('<a>', {
     href: '..',
     html: 'Back'
   }));
-}
-
-function addCss(href) {
-  var css = document.createElement("link");
-  css.href = href;
-  css.rel = "stylesheet";
-  css.type= "text/css";
-
-  document.head.appendChild(css);
-}
-
-function addScript(src, callback) {
-  var script = document.createElement("script");
-  script.type = "text/javascript";
-  script.src = src;
-
-  if (callback) {
-    // inspired by https://gist.github.com/hagenburger/500716
-    var loaded;
-
-    script.onreadystatechange = script.onload = function() {
-      if (!loaded) {
-        callback();
-      }
-      loaded = true;
-    };
-  }
-
-  document.body.appendChild(script);
 }
